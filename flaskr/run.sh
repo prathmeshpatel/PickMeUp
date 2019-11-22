79a87351ba35de6da87d99857b6269b4585f11f2
prathmeshpatel <prathmesh.patel@bright-wolf.com>
2019-11-21 23:05:33 -0500

    Kyle and I added User state functionality and made significant revisions to SQL schema

--- flaskr/run.sh
+++ flaskr/run.sh
@@ -2,6 +2,8 @@
 function usage() {
     echo "Usage: sh ${0} create.db"
 }
+export FLASK_APP=flaskr
+export FLASK_DEBUG=true
 if [ "$#" -ne 1 ]; then
     usage
     exit 1
@@ -13,6 +15,4 @@ else
     echo "DB does not exist, initializing db."
     flask initdb
 fi
-export FLASK_APP=flaskr
-export FLASK_DEBUG=true
 flask run
