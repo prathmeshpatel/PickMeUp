a29fdee886fdc639438cfe64aef05371ac56739f
Brooke Erickson <bce14@duke.edu>
2019-12-07 18:18:03 -0500

    update load.sql with new schema

--- flaskr/load.sql
+++ flaskr/load.sql
@@ -1,10 +1,10 @@
 INSERT INTO RegisteredUser VALUES ('bce14@duke.edu', 'Brooke Erickson','brooke');
 INSERT INTO RegisteredUser VALUES ('m.kylemurphy@gmail.com', 'Kyle Murphy', 'kyle');
 
-INSERT INTO Sleep VALUES ('bce14@duke.edu','2019-10-06','00:00:00','7:00:00',7);
-INSERT INTO Sleep VALUES ('m.kylemurphy@gmail.com','2019-10-06','00:00:00','8:00:00',8);
-INSERT INTO Sleep VALUES ('bce14@duke.edu','2019-10-07','00:00:00','9:00:00',9);
-INSERT INTO Sleep VALUES ('m.kylemurphy@gmail.com','2019-10-07','00:00:00','8:00:00',8);
+INSERT INTO Sleep VALUES ('bce14@duke.edu','2019-10-06','00:00:00','7:00:00',3);
+INSERT INTO Sleep VALUES ('m.kylemurphy@gmail.com','2019-10-06','00:00:00','8:00:00',5);
+INSERT INTO Sleep VALUES ('bce14@duke.edu','2019-10-07','00:00:00','9:00:00',4);
+INSERT INTO Sleep VALUES ('m.kylemurphy@gmail.com','2019-10-07','00:00:00','8:00:00',4);
 
 INSERT INTO Exercise VALUES ('bce14@duke.edu','2019-10-06','07:00:00','8:00:00',1);
 INSERT INTO Exercise VALUES ('m.kylemurphy@gmail.com','2019-10-06','08:00:00','9:00:00',1);
@@ -16,20 +16,20 @@ INSERT INTO Work VALUES ('m.kylemurphy@gmail.com','09:00:00','13:00:00',4);
 INSERT INTO Work VALUES ('bce14@duke.edu','2019-10-06','08:00:00','12:00:00',4);
 INSERT INTO Work VALUES ('m.kylemurphy@gmail.com','09:00:00','13:00:00',4);
 
-INSERT INTO Meals VALUES ('bce14@duke.edu','2019-10-06','12:00:00','13:00:00',1);
+INSERT INTO Meals VALUES ('bce14@duke.edu','2019-10-06','12:00:00','13:00:00',4);
 INSERT INTO Meals VALUES ('m.kylemurphy@gmail.com','2019-10-06','13:00:00','14:00:00',1);
-INSERT INTO Meals VALUES ('bce14@duke.edu','2019-10-07','14:00:00','14:30:00',0.5);
-INSERT INTO Meals VALUES ('m.kylemurphy@gmail.com','2019-10-07','12:00:00','13:00:00',1);
+INSERT INTO Meals VALUES ('bce14@duke.edu','2019-10-07','14:00:00','14:30:00',3);
+INSERT INTO Meals VALUES ('m.kylemurphy@gmail.com','2019-10-07','12:00:00','13:00:00',5);
 
 INSERT INTO Social VALUES ('bce14@duke.edu','2019-10-06','20:00:00','23:00:00',3);
 INSERT INTO Social VALUES ('m.kylemurphy@gmail.com','2019-10-06','18:00:00','23:00:00',5);
 INSERT INTO Social VALUES ('bce14@duke.edu','2019-10-07','19:00:00','23:00:00',4);
 INSERT INTO Social VALUES ('m.kylemurphy@gmail.com','2019-10-07','20:00:00','23:00:00',3);
 
-INSERT INTO Downtime VALUES ('bce14@duke.edu','2019-10-06','13:00:00','20:00:00',7);
+INSERT INTO Downtime VALUES ('bce14@duke.edu','2019-10-06','13:00:00','20:00:00',3);
 INSERT INTO Downtime VALUES ('m.kylemurphy@gmail.com','2019-10-06','14:00:00','18:00:00',4);
-INSERT INTO Downtime VALUES ('bce14@duke.edu','2019-10-07','14:30:00','19:00:00',3.5);
-INSERT INTO Downtime VALUES ('m.kylemurphy@gmail.com','2019-10-07','13:00:00','20:00:00',7);
+INSERT INTO Downtime VALUES ('bce14@duke.edu','2019-10-07','14:30:00','19:00:00',3);
+INSERT INTO Downtime VALUES ('m.kylemurphy@gmail.com','2019-10-07','13:00:00','20:00:00',5);
 
 INSERT INTO Mood VALUES ('bce14@duke.edu','2019-10-06',5);
 INSERT INTO Mood VALUES ('m.kylemurphy@gmail.com','2019-10-06',4);
